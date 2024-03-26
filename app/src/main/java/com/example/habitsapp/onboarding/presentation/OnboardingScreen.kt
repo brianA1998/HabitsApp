97bfcc0c09439844a8bbe6b8b8213846387c1531
brianA1998 <brian.alaniz@hotmail.com>
2024-03-26 01:34:53 -0300

    add screens and model

--- app/src/main/java/com/example/habitsapp/onboarding/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/example/habitsapp/onboarding/presentation/OnboardingScreen.kt
@@ -1,14 +1,24 @@
 package com.example.habitsapp.onboarding.presentation
 
 import androidx.compose.runtime.Composable
+import com.example.habitsapp.R
 import com.example.habitsapp.onboarding.presentation.components.OnboardingPager
 
 @Composable
 fun OnboardingScreen(){
     val pages = listOf(
-        "titulo 1",
-        "titulo 2",
-        "titulo 3"
+      OnboardingPagerInformation("Welcome to Monumental Habits",
+          "We can help you to be a better version of yourself",
+          R.drawable.onboarding1),
+        OnboardingPagerInformation("Create new habits easily",
+            "We can help you to be a better version of yourself",
+            R.drawable.onboarding2),
+        OnboardingPagerInformation("Keep track of your progress",
+            "We can help you to be a better version of yourself",
+            R.drawable.onboarding3),
+        OnboardingPagerInformation("Join a supportive community",
+            "We can help you to be a better version of yourself",
+            R.drawable.onboarding4)
     )
     
     OnboardingPager(pages = pages, onFinish = { /*TODO*/ })
--- app/src/main/java/com/example/habitsapp/onboarding/presentation/components/OnboardingPager.kt
+++ app/src/main/java/com/example/habitsapp/onboarding/presentation/components/OnboardingPager.kt
@@ -1,48 +1,92 @@
 package com.example.habitsapp.onboarding.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.habitsapp.R
+import com.example.habitsapp.onboarding.presentation.OnboardingPagerInformation
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun OnboardingPager(
-    pages: List<String>,
+    pages: List<OnboardingPagerInformation>,
     onFinish: () -> Unit,
     modifier: Modifier = Modifier
 ){
     val pagerState = rememberPagerState()
-    Box(modifier = Modifier){
+
+    val coroutineScope = rememberCoroutineScope()
+    Box(modifier = modifier.background(Color.White)){
         HorizontalPager(count = pages.size, state = pagerState) { index ->
+            val information = pages[index]
             Column(modifier = Modifier
                 .fillMaxSize()
                 .padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally){
                 Spacer(modifier = Modifier.height(32.dp))
-                Text("Titulo")
+                Text(information.title.uppercase())
                 Spacer(modifier = Modifier.height(32.dp))
-                Image(painter = painterResource(id = R.drawable.onboarding1), contentDescription = "onboarding")
+                Image(painter = painterResource(id = information.image),
+                    contentDescription = "onboarding",
+                    modifier = Modifier.aspectRatio(1f),
+                contentScale = ContentScale.FillHeight)
                 Spacer(modifier = Modifier.height(32.dp))
-                Text("Subtitle")
+                Text(information.subtitle.uppercase(),
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                ), textAlign = TextAlign.Center)
             }
         }
-        Row(modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter).padding(64.dp, 16.dp,16.dp), verticalAlignment = Alignment.CenterVertically) {
-            
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 64.dp, start = 16.dp, end = 16.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+
+            if(pagerState.currentPage == pages.lastIndex){
+                Button(onClick = {onFinish}){
+                    Text("Get Started")
+                }
+            }else{
+                TextButton(onClick = { onFinish}){
+                    Text("Skip", color = MaterialTheme.colorScheme.tertiary)
+                }
+
+                HorizontalPagerIndicator(pagerState = pagerState,
+                activeColor = MaterialTheme.colorScheme.tertiary,
+                inactiveColor = MaterialTheme.colorScheme.primary)
+                TextButton(onClick = { coroutineScope.launch {
+                    pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                }}){
+                    Text("Next", color = MaterialTheme.colorScheme.tertiary)
+                }
+            }
         }
     }
 }
