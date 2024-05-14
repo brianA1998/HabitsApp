18e73414fdf6890154478deeadd532d8fcdc4283
brianA1998 <brian.alaniz@hotmail.com>
2024-05-14 00:43:56 -0300

    remove text

--- app/src/main/java/com/example/habitsapp/home/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/habitsapp/home/presentation/home/HomeScreen.kt
@@ -1,22 +1,33 @@
 package com.example.habitsapp.home.presentation.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.habitsapp.R
+import com.example.habitsapp.home.presentation.home.components.HomeDateSelector
 import com.example.habitsapp.home.presentation.home.components.HomeQuote
+import java.time.ZonedDateTime
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,6 +52,22 @@ fun HomeScreen() {
                 author = "Anonymous",
                 imageId = R.drawable.onboarding1
             )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Habits".uppercase(),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                HomeDateSelector(
+                    selectedDate = ZonedDateTime.now(),
+                    mainDate = ZonedDateTime.now(),
+                    onDateClick = {})
+            }
         }
     }
 }
