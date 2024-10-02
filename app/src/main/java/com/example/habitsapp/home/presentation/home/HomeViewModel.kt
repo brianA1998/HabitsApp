579f8b711e9d3873d719e2331f9cf5fa04c6857f
brianA1998 <brian.alaniz@hotmail.com>
2024-10-01 22:14:57 -0300

    migration from groovy to gradle

--- app/src/main/java/com/example/habitsapp/home/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/habitsapp/home/presentation/home/HomeViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.habitsapp.home.domain.home.usecase.HomeUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -17,6 +18,8 @@ class HomeViewModel @Inject constructor(
 ) : ViewModel() {
     var state by mutableStateOf(HomeState())
         private set
+
+    private var currentDayJob: Job? = null
 
     init {
         getHabits()
@@ -44,7 +47,8 @@ class HomeViewModel @Inject constructor(
     }
 
     private fun getHabits() {
-        viewModelScope.launch {
+        currentDayJob?.cancel()
+        currentDayJob = viewModelScope.launch {
             homeUseCases.getHabitsForDateUseCase(state.selectedDate).collectLatest {
                 state = state.copy(
                     habits = it
